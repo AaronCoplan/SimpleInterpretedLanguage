cd34aec67ebce8b7fd0bcf07f803dbf4c229c1b0
AaronCoplan <afcoplan@gmail.com>
2018-08-18 22:23:47 -0400

    Adding basic setup for functions.

--- src/main/java/com/aaroncoplan/module1/SimpleInterpreter1.java
+++ src/main/java/com/aaroncoplan/module1/SimpleInterpreter1.java
@@ -14,6 +14,38 @@ public class SimpleInterpreter1 implements SimpleInterpreter {
 
     @Override
     public void run(String code) {
-        System.out.println(code);
+        String[] lines = code.split("\n");
+        for(String line : lines) {
+            runLine(line);
+            stack.clear();
+        }
     }
+
+    private void runLine(String line) {
+        String[] pieces = line.split(" ");
+        for(String piece : pieces) {
+            if(isFunction(piece)) {
+                executeFunction(piece);
+            } else {
+                stack.push(piece);
+            }
+        }
+    }
+
+    public boolean isFunction(String piece) {
+        if("add".equals(piece)) {
+            return true;
+        } else if("multiply".equals(piece)) {
+            return true;
+        } else if("print".equals(piece)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void executeFunction(String functionName) {
+        System.out.format("Executing '%s'", functionName).println();
+    }
+
 }
